6f58ff621800082782e7fe9abd10b153b03b2023
yuche <i@yuche.me>
2019-02-27 15:13:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,312 @@
+const userCases = {
+  '微信小程序': [
+    {
+      name: '京东购物',
+      url: 'http://storage.jd.com/taro-resource/cases/京东购物.png',
+    },
+    {
+      name: 'TOPLIFE',
+      url: 'http://storage.jd.com/taro-resource/cases/TOPLIFE.png',
+    },
+    {
+      name: '7FRESH',
+      url: 'http://storage.jd.com/taro-resource/cases/image20.jpeg',
+    },
+    {
+      name: '玲珑',
+      url: 'http://storage.jd.com/taro-resource/cases/玲珑.png',
+    },
+    {
+      name: '一起有局',
+      url: 'http://storage.jd.com/taro-resource/cases/一起有局.png',
+    },
+    {
+      name: '荐一物',
+      url: 'http://storage.jd.com/taro-resource/cases/jian.png',
+    },
+    {
+      name: '壹赫兹',
+      url: 'http://storage.jd.com/taro-resource/cases/hz.png',
+    },
+    {
+      name: '京品百货',
+      url: 'http://storage.jd.com/taro-resource/cases/image4.png',
+    },
+    {
+      name: 'Taro UI',
+      url: 'http://storage.jd.com/taro-resource/cases/image16.jpeg',
+    },
+    {
+      name: '吐个槽社区',
+      url: 'http://storage.jd.com/taro-resource/cases/image3.png',
+    },
+    {
+      name: '轻松问卷',
+      url: 'http://storage.jd.com/taro-resource/cases/image5.png',
+    },
+    {
+      name: '喵呜部落',
+      url: 'http://storage.jd.com/taro-resource/cases/image4.jpeg',
+    },
+    {
+      name: '小鱼的信',
+      url: 'http://storage.jd.com/taro-resource/cases/image6.jpeg',
+    },
+    {
+      name: '罗网部落',
+      url: 'http://storage.jd.com/taro-resource/cases/image2.jpeg',
+    },
+    {
+      name: '学区房',
+      url: 'http://storage.jd.com/taro-resource/cases/image8.jpeg',
+    },
+    {
+      name: '还呗申请',
+      url: 'http://storage.jd.com/taro-resource/cases/image18.jpeg',
+    },
+    {
+      name: '多趣游戏盒',
+      url: 'http://storage.jd.com/taro-resource/cases/image23.jpeg',
+    },
+    {
+      name: '妈妈医选',
+      url: 'http://storage.jd.com/taro-resource/cases/image29.jpeg',
+    },
+    {
+      name: 'uCourse',
+      url: 'http://storage.jd.com/taro-resource/cases/image9.jpeg',
+    },
+    {
+      name: '作业部落',
+      url: 'http://storage.jd.com/taro-resource/cases/image7.jpeg',
+    },
+    {
+      name: '开始喝茶',
+      url: 'http://storage.jd.com/taro-resource/cases/image10.jpeg',
+    },
+    {
+      name: '多肉宝宝',
+      url: 'http://storage.jd.com/taro-resource/cases/image11.jpeg',
+    },
+    {
+      name: '工资算一算',
+      url: 'http://storage.jd.com/taro-resource/cases/image12.jpeg',
+    },
+    {
+      name: '精巧待办事项',
+      url: 'http://storage.jd.com/taro-resource/cases/image13.jpeg',
+    },
+    {
+      name: '君博教育',
+      url: 'http://storage.jd.com/taro-resource/cases/image24.jpeg',
+    },
+    {
+      name: '吉珠小助手',
+      url: 'http://storage.jd.com/taro-resource/cases/image28.jpeg',
+    },
+    {
+      name: '掌 buy',
+      url: 'http://storage.jd.com/taro-resource/cases/image14.jpeg',
+    },
+    {
+      name: '脑洞阅读',
+      url: 'http://storage.jd.com/taro-resource/cases/image3.jpeg',
+    },
+    {
+      name: '指尖推',
+      url: 'http://storage.jd.com/taro-resource/cases/image1.png',
+    },
+    {
+      name: '良乌深触',
+      url: 'http://storage.jd.com/taro-resource/cases/image17.jpeg',
+    },
+    {
+      name: '猫眼试用',
+      url: 'http://storage.jd.com/taro-resource/cases/maoyan.png',
+    },
+    {
+      name: '新笑傲福利站',
+      url: 'http://storage.jd.com/taro-resource/cases/image21.jpeg',
+    },
+    {
+      name: '万邦法务',
+      url: 'http://storage.jd.com/taro-resource/cases/image25.jpeg',
+    },
+    {
+      name: '我要个性',
+      url: 'http://storage.jd.com/taro-resource/cases/image27.jpeg',
+    },
+    {
+      name: '时装衣橱',
+      url: 'http://storage.jd.com/taro-resource/cases/image22.jpeg',
+    },
+    {
+      name: '税扣扣',
+      url: 'http://storage.jd.com/taro-resource/cases/image26.jpeg',
+    },
+    {
+      name: '小来打卡 Pro',
+      url: 'http://storage.jd.com/taro-resource/cases/image5.jpeg',
+    },
+    {
+      name: '精雀赛事',
+      url: 'http://storage.jd.com/taro-resource/cases/image31.jpeg',
+    },
+    {
+      name: '车轮社区',
+      url: 'http://storage.jd.com/taro-resource/cases/image32.jpeg',
+    },
+    {
+      name: '斗店',
+      url: 'http://storage.jd.com/taro-resource/cases/image33.jpeg',
+    },
+    {
+      name: '低碳打卡',
+      url: 'http://storage.jd.com/taro-resource/cases/image34.jpeg',
+    },
+    {
+      name: '优投房',
+      url: 'http://storage.jd.com/taro-resource/cases/image35.jpeg',
+    },
+    {
+      name: '湖人赛程',
+      url: 'http://storage.jd.com/taro-resource/cases/image30.jpeg',
+    },
+    {
+      name: '印象分',
+      url: 'http://storage.jd.com/taro-resource/cases/image19.jpeg',
+    },
+    {
+      name: '秀设计',
+      url: 'http://storage.jd.com/taro-resource/cases/image15.jpeg',
+    },
+    {
+      name: '戒烟助理',
+      url: 'https://github.com/hugetiny/quit-smoking/raw/master/weappcode.jpg',
+    },
+    {
+      name: '赛后',
+      url: 'https://user-images.githubusercontent.com/3993655/50722899-28e93880-1111-11e9-8647-3761b59926a1.jpg',
+    },
+    {
+      name: 'Gitter',
+      url: 'https://user-images.githubusercontent.com/8692455/51429898-b159f400-1c4e-11e9-91a1-59cd1fab5042.png',
+    },
+    {
+      name: '前端小助手',
+      url: 'https://user-images.githubusercontent.com/8676711/51724267-84b33b80-2097-11e9-98ed-b4cf94aacb5b.jpg',
+    },
+    {
+      name: '尾巴生活',
+      url: 'https://user-images.githubusercontent.com/25095653/51814579-ecb38d00-22f6-11e9-8b45-9f3505baace6.jpg',
+    },
+    {
+      name: '时时慢',
+      url: 'https://user-images.githubusercontent.com/1301338/51829422-73cd2900-2328-11e9-9a8f-443d9c1eff15.jpg',
+    },
+    {
+      name: '最佳专辑',
+      url: 'https://user-images.githubusercontent.com/1298241/51992353-cdc81d00-24e7-11e9-98ea-17f32aa873cb.jpg',
+    },
+    {
+      name: 'Unsplash',
+      url: 'https://user-images.githubusercontent.com/18414813/52836944-b152f400-3127-11e9-8891-db950c3e7c85.jpeg',
+    },
+    {
+      name: '爱享到',
+      url: 'https://user-images.githubusercontent.com/14814560/52850000-63e97d80-314d-11e9-8e03-57e15d17900d.jpg',
+    },
+  ],
+  'H5': [
+    {
+      name: '作业部落',
+      url: 'https://qr.api.cli.im/qr?data=https%253A%252F%252Fwx.zy.com%252Fmpzuoye%252F&level=H&transparent=false&bgcolor=%23ffffff&forecolor=%23000000&blockpixel=12&marginblock=1&logourl=&size=260&kid=cliim&key=7888904f61917032354a9e5f911ab0bd'
+    },
+    {
+      name: '猫眼电影',
+      url: 'https://user-images.githubusercontent.com/15929863/43410114-6ba5ae00-9458-11e8-8f2a-e2ab9d8c4141.png'
+    },
+    {
+      name: '指尖推',
+      url: 'http://storage.jd.com/taro-resource/cases/zhijian_h5.png'
+    },
+    {
+      name: '时装衣橱',
+      url: 'https://github.com/EasyTuan/taro-msparis/raw/master/screenshots/qr-code.png'
+    },
+    {
+      name: '仿网易严选',
+      url: 'https://camo.githubusercontent.com/b0f85f75689c94ebf935d1d924d73f381153277e/68747470733a2f2f7169742d7465616d2e6769746875622e696f2f7075626c69632f696d616765732f7461726f2d79616e7875616e2f68352d71722d636f64652e706e67'
+    },
+  ],
+  '支付宝小程序': [
+    {
+      name: '时光简纸',
+      url: 'https://user-images.githubusercontent.com/30850497/52847223-8deb7180-3146-11e9-90f4-e668233284f9.jpg'
+    },
+    {
+      name: '爱享到',
+      url: 'https://user-images.githubusercontent.com/30850497/52847223-8deb7180-3146-11e9-90f4-e668233284f9.jpg'
+    },
+  ],
+  '百度智能小程序': [
+    {
+      name: '京东好物街',
+      url: 'http://storage.jd.com/taro-resource/cases/haowujie.png'
+    }
+  ],
+  '移动端应用': [
+    {
+      name: '仿网易严选',
+      url: 'https://qr.api.cli.im/qr?data=https%253A%252F%252Fsnack.expo.io%252F%2540caiminxing%252Ftaro-yanxuan&level=H&transparent=false&bgcolor=%23ffffff&forecolor=%23000000&blockpixel=12&marginblock=1&logourl=&size=260&kid=cliim&key=ad14cc9c74800daec783744be81d68d8'
+    }
+  ]
+}
+
+function chunkArray(ary) {
+  const tempAry = []
+  const MAX_COLUMN = 8
+  for (let i = 0; i < ary.length; i += MAX_COLUMN) {
+    tempAry.push(ary.slice(i, i + MAX_COLUMN))
+  }
+  if (tempAry[0].length < MAX_COLUMN) {
+    tempAry[0].push(...Array(MAX_COLUMN - tempAry[0].length).fill({}))
+  }
+  return tempAry
+}
+
+function renderCases (cases) {
+  return Object.keys(cases).map(key => {
+    return `
+      <h2>${key}</h2>
+      <table
+        border="0"
+        cellpadding="0"
+        cellspacing="0"
+      >
+      <tbody>
+        ${renderTr(cases[key])}
+      </tbody>
+      </table>
+    `
+  }).join('')
+}
+
+function renderTr (arr) {
+  return chunkArray(arr).map(a => {
+    return `
+      <tr>
+        ${a.map(o => {
+          return `
+            <td align="center">
+              <img class="${o.url ? '' : 'hidden'}" height="80" width="80" src="${o.url}" />
+              <p class="${o.url ? '' : 'hidden'}">${o.name}</p>
+            </td>
+          `
+        }).join('')}
+      </tr>
+    `
+  }).join('')
+}
+
+document.getElementById("app").innerHTML = renderCases(userCases)
