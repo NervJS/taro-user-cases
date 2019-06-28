d1d3d211d588317955c61d12404618126790c3e3
yuuk <645481746@qq.com>
2019-06-28 11:06:19 +0800

    add case

--- index.js
+++ index.js
@@ -392,6 +392,10 @@ const userCases = {
       name: "数字酋长客服",
       url:
         "https://user-images.githubusercontent.com/7202516/59986775-c0642880-966a-11e9-83d9-79f66087d1f2.jpg"
+    },
+    {
+      name: "麦德龙到家",
+      url: "https://user-images.githubusercontent.com/9426456/60314192-5a421300-9994-11e9-9448-a0806a975263.jpg"
     }
   ],
   H5: [
@@ -433,10 +437,6 @@ const userCases = {
       name: "学伴",
       url:
         "https://user-images.githubusercontent.com/7202516/58466807-06fa5c00-816d-11e9-81ec-9be385d8a4d1.png"
-    },
-    {
-      name: "麦德龙到家",
-      url: "https://user-images.githubusercontent.com/9426456/60314192-5a421300-9994-11e9-9448-a0806a975263.jpg"
     }
   ],
   支付宝小程序: [
