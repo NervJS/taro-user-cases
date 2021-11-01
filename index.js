26c6c146714a5588219d6592040133a3b6c52efa
tenth-person <46011036+tenth-person@users.noreply.github.com>
2021-11-01 21:36:00 +0800

    Update index.js (#135)

--- index.js
+++ index.js
@@ -86,21 +86,28 @@ const userCases = {
     },
     {
       name: "迅蜂商祺",
-      url: "https://xf-auth.oss-cn-shanghai.aliyuncs.com/applet/gh_3ae6fa1c17c1_1280.jpg",
+      url: "https://xf-qrcode.oss-cn-chengdu.aliyuncs.com/applet/gh_3ae6fa1c17c1.jpg",
       framework: "React",
       taroVersion: "3.x",
       screenshot: []
     },
     {
       name: "迅蜂优选",
-      url: "https://xf-auth.oss-cn-shanghai.aliyuncs.com/applet/gh_4c740728e82e_1280.jpg",
+      url: "https://xf-qrcode.oss-cn-chengdu.aliyuncs.com/applet/gh_4c740728e82e.jpg",
       framework: "React",
       taroVersion: "2.x",
       screenshot: []
     },
     {
       name: "迅蜂商城",
-      url: "https://xf-auth.oss-cn-shanghai.aliyuncs.com/applet/gh_17a20461afb7_1280.jpg",
+      url: "https://xf-qrcode.oss-cn-chengdu.aliyuncs.com/applet/gh_17a20461afb7.jpg",
+      framework: "React",
+      taroVersion: "2.x",
+      screenshot: []
+    },
+    {
+      name: "圈圈蜂抢",
+      url: "https://xf-qrcode.oss-cn-chengdu.aliyuncs.com/applet/gh_6ae3a47e02b8.jpg",
       framework: "React",
       taroVersion: "2.x",
       screenshot: []
@@ -1242,7 +1249,7 @@ const userCases = {
       framework: "react",
       taroVersion: "3.0.21",
       screenshot: ['https://github.com/gwt9502/youhaoquan/blob/main/screen/course.gif']
-    }, 
+    },
     {
       name: "出租车助老出行",
       url: "https://static.didapinche.com/pics/g/6xtxf4q761zhulaochuxing.png",
