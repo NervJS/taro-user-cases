b44f4348010dee8625a657f8a6fde803ab0a3087
FoneQinrf <15921712019@163.com>
2023-06-16 11:00:34 +0800

    Update index.js
    新增Taro开发作品

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const userCases = {
   微信小程序: [
     {
+      name: "baby喂养记录",
+      url: "https://mini-app-project-1302387394.cos.ap-nanjing.myqcloud.com/mini-baby%2F3EAC2DC0-B37B-473e-9968-DA07749A0647.png",
+      framework: "vue",
+      taroVersion: "3.6.5",
+      screenshot: []
+    },
+    {
       name: "简小记",
       url: "https://img.briefnote.cn/8.31171a22610d8-4b5.a3c05caac4d4",
       framework: "react",
